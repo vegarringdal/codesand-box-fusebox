0a0123fed09f1d2b4eee2b69c3fed57985ae1ef2
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-05-07 16:31:54 +0200

    update

--- fuse.js
+++ fuse.js
@@ -1,33 +1,47 @@
-const { fusebox, sparky, pluginPostCSS } = require("fuse-box");
+const { fusebox, pluginPostCSS } = require("fuse-box");
 const { pluginTypeChecker } = require("fuse-box-typechecker");
-
+console.log(process.version);
 let Context = {
-  getConfig: function (prod) {
+  getConfig: function(prod) {
     return fusebox({
       target: "browser",
       entry: `./src/index.ts`,
       webIndex: {
         template: `./src/index.html`,
+        publicPath: "./"
+      },
+      cache: {
+        root: `./.cache/`,
+        enabled: !prod
+      },
+      watcher: {
+        enabled: !prod,
+        include: [`./src/`],
+        ignored: ["dist"]
       },
       hmr: { plugin: `./src/fuseHmrPlugin.ts` },
-      devServer:true,
+      devServer: { httpServer: { port: 8080 } },
       plugins: [
         pluginPostCSS(/\.css$/, {
           stylesheet: {
             postCSS: {
-              plugins: [require("tailwindcss"), require("autoprefixer")],
-            },
-          },
+              plugins: [require("tailwindcss"), require("autoprefixer")]
+            }
+          }
         }),
         pluginTypeChecker({
           basePath: "./",
           tsConfigOverride: {
             extends: "./tsconfig.json",
-          },
-        }),
-      ],
+            compilerOptions: {
+              rootDirs: [`./src`]
+            },
+            exclude: ["**/__tests__"]
+          }
+        })
+      ]
     });
-  },
+  }
 };
 
 // commen runner for all samples
@@ -36,7 +50,8 @@ async function run(ctx) {
 
   const fuse = ctx.getConfig(false);
   await fuse.runDev({
-    bundles: { distRoot: `./dist/`, app: "app.js" },
+    port: 4444,
+    bundles: { distRoot: `./dist/`, app: "app.js" }
   });
 }
 
